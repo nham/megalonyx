0c68b1e0d5d3eebdaf9c42ff169da92fd8c1cf80
nham <hamann.nick@gmail.com>
2014-08-15 21:44:43 -0400

    Make try_match a method. Add Zilch variant to Value enum

--- src/mod.rs
+++ src/mod.rs
@@ -13,7 +13,9 @@ enum ParsingExpr<A, N> {
 }
 
 
+#[deriving(PartialEq, Eq, Clone)]
 enum Value<A> {
+    Zilch,
     At(A),
     List(Vec<Value<A>>),
 }
@@ -26,10 +28,27 @@ type MatchReturn<'a, A> = (Value<A>, Values<'a, A>);
 
 type Values<'a, A> = &'a [Value<A>];
 
-fn try_match<'a, A, N>(e: ParsingExpr<A, N>, input: Values<'a, A>) 
-    -> Result<MatchReturn<'a, A>, ()> {
-        Err(())
+impl<A, N> Grammar<A, N> {
+    fn try_match<'a, A, N>(&self, e: ParsingExpr<A, N>, input: Values<'a, A>)
+        -> Result<MatchReturn<'a, A>, ()>
+        where A: Clone + Eq {
 
+        match e {
+            Nil => Ok((Zilch, input)),
+            Atomic(a) => {
+                match input {
+                    [ref b, ..rest] if *b == At(a) => Ok((b.clone(), rest)),
+                    _ => Err(()),
+                }
+            },
+            Nonterminal(n) => {
+                //match try_match(
+                fail!("Unimplemented");
+
+            },
+            _ => fail!("Unimplemented"),
+        }
+    }
 }
 
 fn main() {
